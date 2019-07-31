320e9cc1333559230490b0b7d7b3b507cd153f90
Martuza Shimul <martuza.shimul@gmail.com>
2019-08-01 02:13:15 +0600

    error handeling added

--- public/js/app.js
+++ public/js/app.js
@@ -26,16 +26,16 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading....'
     messageTwo.textContent = ''
     fetch('http://localhost:3000/weather?address='+location).then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            // console.log(data.error)
-            messageOne.textContent = data.error
-        } else {
-            // console.log(data.location)
-            // console.log(data.forecast)
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        }
+        response.json().then((data) => {
+            if(data.error){
+                // console.log(data.error)
+                messageOne.textContent = data.error
+            } else {
+                // console.log(data.location)
+                // console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
-})
 })
